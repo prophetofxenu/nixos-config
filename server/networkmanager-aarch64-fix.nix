8a9544cc0f20542a22bfeeaf1fa45834d917b4a9
prophetofxenu <xenu@prophetofxenu.net>
2026-02-23 18:57:48 -0500

    fix formatting

--- server/networkmanager-aarch64-fix.nix
+++ server/networkmanager-aarch64-fix.nix
@@ -2,15 +2,15 @@
 
 {
   nixpkgs.overlays = [
-(final: prev:
-{
-  networkmanager = prev.networkmanager.overrideAttrs (oldAttrs: {
-    
-  mesonFlags = prev.networkmanager.mesonFlags ++ [
-    "-Dman=${prev.lib.boolToString (prev.stdenv.buildPlatform == prev.stdenv.hostPlatform)}"
-  ];
+    (final: prev:
+     {
+     networkmanager = prev.networkmanager.overrideAttrs (oldAttrs: {
 
-  });
-})
-];
+         mesonFlags = prev.networkmanager.mesonFlags ++ [
+         "-Dman=${prev.lib.boolToString (prev.stdenv.buildPlatform == prev.stdenv.hostPlatform)}"
+         ];
+
+         });
+     })
+  ];
 }
